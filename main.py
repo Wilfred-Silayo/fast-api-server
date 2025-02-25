27d2907a45b90b39b70b0e23a0bdc463293e96de
Wilfred Silayo <wilfredsilayo99@gmail.com>
2025-02-25 10:45:23 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, BackgroundTasks
+import uvicorn
+import time
+
+app = FastAPI()
+
+@app.get('/')
+def index():
+    data = {
+        "name": "wilfred silayo",
+        "age": 26,
+        "marital status": "single",
+        "profession": "software developer"
+    }
+    return data
+
+def send_email(email: str, message: str):
+    time.sleep(5)  # Simulate email processing time
+    print(f"Email sent to {email}: {message}")
+
+@app.post("/send-email/")
+def trigger_email(email: str, background_tasks: BackgroundTasks):
+    background_tasks.add_task(send_email, email, "Welcome to FastAPI!")
+    return {"message": "Email sending started!"}
+
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host= "127.0.0.1", port= 9000, reload=True)
+
+# uvicorn main:app --host 127.0.0.1 --port 8000 --reload
+#fastapi dev main.py
